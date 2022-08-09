6445aae00d91a60a24ef1b8099e7626f33e4dd9f
andrew <vip.paevskiy@gmail.com>
2022-08-10 00:12:11 +0300

    Add a first version

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(calc(scanner.nextLine()));
+
+    }
+
+    public static String calc(String input) {
+        String res = null;
+        String[] expression = input.split(" ");
+        int a = Integer.parseInt(expression[0]);
+        int b = Integer.parseInt(expression[2]);
+        System.out.printf("%d, %d\n", a, b);
+        if (a < 1 || a > 10 || b < 1 || b > 10) {
+            return "Числа должны быть от 1 до 10 включительно";
+        }
+        res = switch (expression[1]) {
+            case "+" -> String.valueOf(a + b);
+            case "/" -> String.valueOf(a / b);
+            case "*" -> String.valueOf(a * b);
+            case "-" -> String.valueOf(a - b);
+            default -> "Операция не найдена";
+        };
+        return res;
+    }
 }
